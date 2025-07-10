44fd095577b5fa6d8da35ae2e1f4b51411736766
gujunhyeong <rnwnsgud90@naver.com>
2025-07-11 01:23:06 +0900

    [GH-3-facility-query] 시설 목록조회

--- gh-domain/gh-facility/gh-facility-api/src/main/java/kr/co/goldenhome/service/ElderlyFacilityService.java
+++ gh-domain/gh-facility/gh-facility-api/src/main/java/kr/co/goldenhome/service/ElderlyFacilityService.java
@@ -3,9 +3,12 @@ package kr.co.goldenhome.service;
 import exception.CustomException;
 import exception.ErrorCode;
 import kr.co.goldenhome.dto.ElderlyFacilityResponse;
+import kr.co.goldenhome.entity.ElderlyFacility;
 import kr.co.goldenhome.repository.ElderlyFacilityRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -15,5 +18,12 @@ public class ElderlyFacilityService {
 
     public ElderlyFacilityResponse read(Long facilityId) {
         return ElderlyFacilityResponse.from(elderlyFacilityRepository.findById(facilityId).orElseThrow(()-> new CustomException(ErrorCode.NOT_FOUND, "ElderlyFacilityService.read")));
+    }
+
+    public List<ElderlyFacilityResponse> readAll(String facilityType, Long lastId, Long pageSize) {
+        List<ElderlyFacility> elderlyFacilities = lastId == null ?
+                elderlyFacilityRepository.findAllInfiniteScroll(facilityType, pageSize) :
+                elderlyFacilityRepository.findAllInfiniteScroll(facilityType, lastId, pageSize);
+        return elderlyFacilities.stream().map(ElderlyFacilityResponse::from).toList();
     }
 }
--- gh-domain/gh-facility/gh-facility-core/src/main/java/kr/co/goldenhome/repository/ElderlyFacilityRepository.java
+++ gh-domain/gh-facility/gh-facility-core/src/main/java/kr/co/goldenhome/repository/ElderlyFacilityRepository.java
@@ -2,6 +2,28 @@ package kr.co.goldenhome.repository;
 
 import kr.co.goldenhome.entity.ElderlyFacility;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface ElderlyFacilityRepository extends JpaRepository<ElderlyFacility, Long> {
+    @Query(
+            value = "select * " +
+                    "from elderly_facilities " +
+                    "where elderly_facilities.facility_type = :facilityType " +
+                    "limit :limit",
+            nativeQuery = true
+    )
+    List<ElderlyFacility> findAllInfiniteScroll(@Param("facilityType") String facilityType, @Param("limit") Long limit);
+
+
+    @Query(
+            value = "select * " +
+                    "from elderly_facilities " +
+                    "where elderly_facilities.facility_type = :facilityType and elderly_facilities.id > :lastId " +
+                    "limit :limit",
+            nativeQuery = true
+    )
+    List<ElderlyFacility> findAllInfiniteScroll(@Param("facilityType") String facilityType, @Param("lastId") Long lastId, Long limit);
 }

2baafd71d5e4cb091a8715a3123d45e649bb1430
gujunhyeong <rnwnsgud90@naver.com>
2025-07-31 01:28:24 +0900

    [GH-3] Null check

--- gh-domain/gh-facility/gh-facility-api/src/main/java/kr/co/goldenhome/implement/FacilityReader.java
+++ gh-domain/gh-facility/gh-facility-api/src/main/java/kr/co/goldenhome/implement/FacilityReader.java
@@ -1,5 +1,7 @@
 package kr.co.goldenhome.implement;
 
+import exception.CustomException;
+import exception.ErrorCode;
 import kr.co.goldenhome.dto.FacilityCombinedDto;
 import kr.co.goldenhome.dto.FacilityDetailResponse;
 import kr.co.goldenhome.dto.FacilityPhotoResponse;
@@ -24,6 +26,9 @@ public class FacilityReader {
 
     public FacilityDetailResponse read(Long facilityId) {
         FacilityCombinedDto facilityCombinedDto = facilityRepository.read(facilityId);
+        if (facilityCombinedDto == null) {
+            throw new CustomException(ErrorCode.FACILITY_NOT_FOUND, "FacilityReader.read");
+        }
         List<FacilityPhotoResponse> facilityPhotoResponses = facilityPhotoRepository.findByInstitutionSymbol(facilityCombinedDto.institutionSymbol())
                 .stream().map(FacilityPhotoResponse::from).toList();
         List<FacilityProgramResponse> facilityProgramResponses = facilityProgramRepository.findByInstitutionSymbol(facilityCombinedDto.institutionSymbol())

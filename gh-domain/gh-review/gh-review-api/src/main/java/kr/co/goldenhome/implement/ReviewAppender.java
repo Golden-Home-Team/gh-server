45acc619da78e79824b875ee0332a359acea906a
gujunhyeong <rnwnsgud90@naver.com>
2025-08-08 02:45:57 +0900

    [GH-5] 리뷰 작성시 리뷰 수 증가

--- gh-domain/gh-review/gh-review-api/src/main/java/kr/co/goldenhome/implement/ReviewAppender.java
+++ gh-domain/gh-review/gh-review-api/src/main/java/kr/co/goldenhome/implement/ReviewAppender.java
@@ -2,9 +2,12 @@ package kr.co.goldenhome.implement;
 
 import kr.co.goldenhome.ReviewImageApi;
 import kr.co.goldenhome.entity.Review;
+import kr.co.goldenhome.entity.ReviewCount;
+import kr.co.goldenhome.repository.ReviewCountRepository;
 import kr.co.goldenhome.repository.ReviewRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -14,9 +17,15 @@ public class ReviewAppender {
 
     private final ReviewRepository reviewRepository;
     private final ReviewImageApi reviewImageApi;
+    private final ReviewCountRepository reviewCountRepository;
 
+    @Transactional
     public void write(String content, int score, List<String> formattedFileNames, Long facilityId, Long userId) {
         Review review = reviewRepository.save(Review.create(facilityId, userId, content, score));
         if (!formattedFileNames.isEmpty()) reviewImageApi.saveAll(review.getId(), formattedFileNames);
+        int result = reviewCountRepository.increase(facilityId);
+        if (result == 0) {
+            reviewCountRepository.save(ReviewCount.create(facilityId, 1L));
+        }
     }
 }

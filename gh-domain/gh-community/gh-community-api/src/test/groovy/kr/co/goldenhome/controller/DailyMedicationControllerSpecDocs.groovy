c06fac8a31096d6f6f7b59927577822d122018c9
gujunhyeong <rnwnsgud90@naver.com>
2025-08-24 16:23:48 +0900

    [GH-8] 오늘의 복약 - 요일별로 읽기

--- gh-domain/gh-community/gh-community-api/src/test/groovy/kr/co/goldenhome/controller/DailyMedicationControllerSpecDocs.groovy
+++ gh-domain/gh-community/gh-community-api/src/test/groovy/kr/co/goldenhome/controller/DailyMedicationControllerSpecDocs.groovy
@@ -2,6 +2,7 @@ package kr.co.goldenhome.controller
 
 import com.fasterxml.jackson.databind.ObjectMapper
 import kr.co.goldenhome.dto.DailyMedicationRequest
+import kr.co.goldenhome.dto.DailyMedicationResponse
 import kr.co.goldenhome.dto.DailyMedicationUpdateRequest
 import kr.co.goldenhome.service.DailyMedicationService
 import org.spockframework.spring.SpringBean
@@ -17,7 +18,9 @@ import org.springframework.test.web.servlet.request.MockMvcRequestBuilders
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers
 import spock.lang.Specification
 
+import java.time.DayOfWeek
 import java.time.LocalDate
+import java.time.LocalDateTime
 
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document
 import static org.springframework.restdocs.operation.preprocess.Preprocessors.preprocessRequest
@@ -34,6 +37,7 @@ import static org.springframework.restdocs.payload.PayloadDocumentation.requestF
 import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields
 import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName
 import static org.springframework.restdocs.request.RequestDocumentation.pathParameters
+import static org.springframework.restdocs.request.RequestDocumentation.queryParameters
 
 @ActiveProfiles("test")
 @SpringBootTest
@@ -111,6 +115,56 @@ class DailyMedicationControllerSpecDocs extends Specification {
                         responseFields(
                                 fieldWithPath("success").type(JsonFieldType.BOOLEAN)
                                         .description("성공여부")
+                        )
+                ))
+        then:
+        response.andExpect {
+            MockMvcResultMatchers.status().isOk()
+            MockMvcResultMatchers.jsonPath('$.success').value(true)
+        }
+
+    }
+
+    def "오늘의 복약 - 요일단위로 읽기"() {
+        given:
+        def givenFacilityId = 1L
+        def givenResponse = new DailyMedicationResponse(
+                1L,
+                LocalDate.now(),
+                "아침 1회 복용",
+                "점심 2회 복용",
+                "특이사항 없음",
+                LocalDateTime.now()
+        )
+        dailyMedicationService.readByDayOfWeek(givenFacilityId, DayOfWeek.MONDAY) >> givenResponse
+
+        when:
+        def response = mockMvc.perform(MockMvcRequestBuilders.get("/api/communities/{facilityId}/daily-medication", givenFacilityId)
+                .contentType(MediaType.APPLICATION_JSON)
+                .queryParam("dayOfWeek", "MONDAY")
+        )
+                .andDo(document("daily-medication-readByDayOfWeek",
+                        preprocessRequest(prettyPrint()),
+                        preprocessResponse(prettyPrint()),
+                        pathParameters(
+                                parameterWithName("facilityId").description("시설 아이디"),
+                        ),
+                        queryParameters(
+                                parameterWithName("dayOfWeek").description("MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY, SUNDAY")
+                        ),
+                        responseFields(
+                                fieldWithPath("id").type(JsonFieldType.NUMBER)
+                                        .description("오늘의 복약 아이디"),
+                                fieldWithPath("recordDate").type(JsonFieldType.STRING)
+                                        .description("기록일"),
+                                fieldWithPath("morningContent").type(JsonFieldType.STRING)
+                                        .description("오늘의 복약 아이디"),
+                                fieldWithPath("afternoonContent").type(JsonFieldType.STRING)
+                                        .description("오늘의 복약 아이디"),
+                                fieldWithPath("nightContent").type(JsonFieldType.STRING)
+                                        .description("오늘의 복약 아이디"),
+                                fieldWithPath("createdAt").type(JsonFieldType.STRING)
+                                        .description("생성일"),
                         )
                 ))
         then:

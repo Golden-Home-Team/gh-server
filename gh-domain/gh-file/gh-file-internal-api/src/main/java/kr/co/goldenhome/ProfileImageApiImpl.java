fdd4e3fc5c650138a3494eff4cbad4315f43ac36
gujunhyeong <rnwnsgud90@naver.com>
2025-08-30 00:52:16 +0900

    [GH-3] aws sdk v2 마이그레이션

--- gh-domain/gh-file/gh-file-internal-api/src/main/java/kr/co/goldenhome/ProfileImageApiImpl.java
+++ gh-domain/gh-file/gh-file-internal-api/src/main/java/kr/co/goldenhome/ProfileImageApiImpl.java
@@ -1,8 +1,9 @@
 package kr.co.goldenhome;
 
-import com.amazonaws.services.s3.AmazonS3;
-import com.amazonaws.services.s3.model.AmazonS3Exception;
-import com.amazonaws.services.s3.model.DeleteObjectRequest;
+//import com.amazonaws.services.s3.AmazonS3;
+//import com.amazonaws.services.s3.model.AmazonS3Exception;
+//import com.amazonaws.services.s3.model.DeleteObjectRequest;
+import io.awspring.cloud.s3.S3Exception;
 import kr.co.goldenhome.entity.ProfileImage;
 import kr.co.goldenhome.repository.ProfileImageRepository;
 import lombok.RequiredArgsConstructor;
@@ -10,6 +11,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.DeleteObjectRequest;
 
 
 @Slf4j
@@ -22,7 +25,7 @@ public class ProfileImageApiImpl implements ProfileImageApi {
     private String awsBaseUrl;
     @Value("${aws.s3.bucket}")
     private String bucket;
-    private final AmazonS3 amazonS3;
+    private final S3Client s3Client;
 
     @Override
     public ProfileImageApiResponse getByUserId(Long userId) {
@@ -36,12 +39,14 @@ public class ProfileImageApiImpl implements ProfileImageApi {
      * 유저는 인당 1개의 프로필 이미지를 갖는다.
      * 새 이미지는 프론트엔드에서 Presigned URL 로 S3에 이미 업로드된 상태라고 가정한다.
      */
+
     @Override
     @Transactional
     public void save(String formattedImageName, Long userId) {
         String newImageUrl = awsBaseUrl + formattedImageName;
         ProfileImage oldProfileImage = profileImageRepository.findByUserId(userId).orElse(null);
         String oldFormattedName = (oldProfileImage != null) ? oldProfileImage.getFormattedName() : null;
+
         try {
             if (oldProfileImage != null) {
                 profileImageRepository.deleteById(userId);
@@ -50,10 +55,13 @@ public class ProfileImageApiImpl implements ProfileImageApi {
 
             if (oldFormattedName != null) {
                 try {
-                    amazonS3.deleteObject(new DeleteObjectRequest(bucket, oldFormattedName));
+                    DeleteObjectRequest deleteRequest = DeleteObjectRequest.builder()
+                            .bucket(bucket)
+                            .key(oldFormattedName)
+                            .build();
+                    s3Client.deleteObject(deleteRequest);
                     log.info("기존 프로필 이미지 S3 삭제 성공: {}", oldFormattedName);
-                } catch (AmazonS3Exception e) {
-                    // 필요한 경우 실패한 삭제 요청을 큐에 넣어 재시도
+                } catch (S3Exception e) {
                     log.error("기존 프로필 이미지 S3 삭제 실패 (고아 이미지 가능성): {}", oldFormattedName, e);
                     throw e;
                 }
@@ -61,14 +69,19 @@ public class ProfileImageApiImpl implements ProfileImageApi {
         } catch (RuntimeException dbException) {
             log.error("프로필 이미지 DB 업데이트 실패. S3에 업로드된 새 이미지 롤백 시도: {}", formattedImageName, dbException);
             try {
-                amazonS3.deleteObject(new DeleteObjectRequest(bucket, formattedImageName));
+                DeleteObjectRequest deleteRequest = DeleteObjectRequest.builder()
+                        .bucket(bucket)
+                        .key(formattedImageName)
+                        .build();
+                s3Client.deleteObject(deleteRequest);
                 log.info("DB 업데이트 실패로 인해 S3 새 이미지 롤백 성공: {}", formattedImageName);
-            } catch (AmazonS3Exception s3DeleteException) {
+            } catch (S3Exception s3DeleteException) {
                 log.error("DB 트랜잭션 롤백 중 S3 새 프로필 이미지 삭제 실패 (고아 이미지 가능성): {}", formattedImageName, s3DeleteException);
             }
             throw dbException;
         }
 
     }
+
 
 }

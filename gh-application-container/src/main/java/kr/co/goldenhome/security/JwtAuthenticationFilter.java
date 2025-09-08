020e65d3d968ad0c6829c3bdf8ddda483799e4e2
gujunhyeong <rnwnsgud90@naver.com>
2025-09-09 02:27:24 +0900

    [GH-6] 시큐리티 수정

--- gh-application-container/src/main/java/kr/co/goldenhome/security/JwtAuthenticationFilter.java
+++ gh-application-container/src/main/java/kr/co/goldenhome/security/JwtAuthenticationFilter.java
@@ -1,5 +1,6 @@
 package kr.co.goldenhome.security;
 
+import jakarta.servlet.ServletException;
 import kr.co.goldenhome.auth.UserPrincipal;
 import kr.co.goldenhome.exception.CustomException;
 import kr.co.goldenhome.exception.ErrorCode;
@@ -40,39 +41,46 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
     private final UserRepository userRepository;
 
     @Override
-    protected void doFilterInternal(@NonNull HttpServletRequest request, @NonNull HttpServletResponse response, @NonNull FilterChain filterChain) throws IOException {
+    protected void doFilterInternal(@NonNull HttpServletRequest request, @NonNull HttpServletResponse response, @NonNull FilterChain filterChain) throws IOException, ServletException {
         String accessToken = parseToken(request.getHeader("Authorization"));
-        if (!StringUtils.hasText(accessToken)) {
-            sendErrorResponse(response, HttpStatus.UNAUTHORIZED, "토큰이 제공되지 않았습니다.");
-            return;
-        }
-        try {
-            Long userId = authenticationTokenManager.getUserId(accessToken);
-            User user = userRepository.findById(userId).orElseThrow(() -> new CustomException(ErrorCode.NOT_FOUND, "JwtAuthenticationFilter.doFilterInternal"));
-            UserPrincipal userPrincipal = new UserPrincipal(userId);
-            Authentication authenticationToken = new UsernamePasswordAuthenticationToken(
-                    userPrincipal,
-                    user.getLoginId(),
-                    getAuthorities(user)
-            );
-            SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+        List<String> publicPaths = List.of("/api/facilities");
+        boolean isPublicPath = publicPaths.stream().anyMatch(request.getRequestURI()::startsWith);
+        if (StringUtils.hasText(accessToken)) {
+            try {
+                Long userId = authenticationTokenManager.getUserId(accessToken);
+                User user = userRepository.findById(userId).orElseThrow(() -> new CustomException(ErrorCode.NOT_FOUND, "JwtAuthenticationFilter.doFilterInternal"));
+                UserPrincipal userPrincipal = new UserPrincipal(userId);
+                Authentication authenticationToken = new UsernamePasswordAuthenticationToken(
+                        userPrincipal,
+                        user.getLoginId(),
+                        getAuthorities(user)
+                );
+                SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+                filterChain.doFilter(request, response);
+            } catch (ExpiredJwtException e) {
+                sendErrorResponse(response, HttpStatus.UNAUTHORIZED, "토큰이 만료되었습니다.");
+            } catch (MalformedJwtException e) {
+                sendErrorResponse(response, HttpStatus.UNAUTHORIZED, "유효하지 않은 토큰입니다.");
+            } catch (CustomException e) {
+                sendErrorResponse(response, HttpStatus.valueOf(e.getErrorCode().getHttpStatus()), e.getErrorCode().getMessage());
+            } catch (Exception e) {
+                log.error(e.getMessage(), e);
+                sendErrorResponse(response, HttpStatus.INTERNAL_SERVER_ERROR, "인증 중 알 수 없는 오류가 발생했습니다.");
+            }
+        } else {
+            if (!isPublicPath) {
+                sendErrorResponse(response, HttpStatus.UNAUTHORIZED, "토큰이 제공되지 않았습니다.");
+                return;
+            }
             filterChain.doFilter(request, response);
-        } catch (ExpiredJwtException e) {
-            sendErrorResponse(response, HttpStatus.UNAUTHORIZED, "토큰이 만료되었습니다.");
-        } catch (MalformedJwtException e) {
-            sendErrorResponse(response, HttpStatus.UNAUTHORIZED, "유효하지 않은 토큰입니다.");
-        } catch (CustomException e) {
-            sendErrorResponse(response, HttpStatus.valueOf(e.getErrorCode().getHttpStatus()), e.getErrorCode().getMessage());
-        } catch (Exception e) {
-            log.error(e.getMessage(), e);
-            sendErrorResponse(response, HttpStatus.INTERNAL_SERVER_ERROR, "인증 중 알 수 없는 오류가 발생했습니다.");
         }
+
 
     }
 
     @Override
     protected boolean shouldNotFilter(HttpServletRequest request) {
-        String[] excludePath = {"/api/auth", "/api/users/signup", "/api/facilities"};
+        String[] excludePath = {"/api/auth", "/api/users/signup"};
         String path = request.getRequestURI();
         return Arrays.stream(excludePath).anyMatch(path::startsWith) ||
                 path.endsWith(".html");

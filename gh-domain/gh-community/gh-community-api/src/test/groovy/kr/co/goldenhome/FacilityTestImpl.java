3a7a204c53839cb70e5c9b54fa7ffb36fad6e594
gujunhyeong <rnwnsgud90@naver.com>
2025-09-18 15:53:14 +0900

    [GH-8] 테스트 수정

---
+++ gh-domain/gh-community/gh-community-api/src/test/groovy/kr/co/goldenhome/FacilityTestImpl.java
@@ -0,0 +1,11 @@
+package kr.co.goldenhome;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class FacilityTestImpl implements FacilityApi{
+    @Override
+    public FacilityApiResponse get(Long facilityId) {
+        return null;
+    }
+}

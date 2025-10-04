61aeb48d91cf12a0d6a3fa024e1b87565d5c9608
gujunhyeong <rnwnsgud90@naver.com>
2025-10-04 22:14:20 +0900

    [GH-3] 채팅 개발 중

--- gh-domain/gh-chat/gh-chat-api/src/main/java/kr/co/goldenhome/service/ChatService.java
+++ gh-domain/gh-chat/gh-chat-api/src/main/java/kr/co/goldenhome/service/ChatService.java
@@ -3,12 +3,11 @@ package kr.co.goldenhome.service;
 import kr.co.goldenhome.entity.ChatMessage;
 import kr.co.goldenhome.exception.CustomException;
 import kr.co.goldenhome.exception.ErrorCode;
-import kr.co.goldenhome.messaging.ChatMessageProducer;
+import kr.co.goldenhome.messaging.ChatStreamWriter;
 import kr.co.goldenhome.messaging.ChatRoomCreatedEvent;
 import kr.co.goldenhome.CommunityApi;
 import kr.co.goldenhome.implement.ChatManager;
 import lombok.RequiredArgsConstructor;
-import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Service;
 
 import java.util.Objects;
@@ -19,18 +18,17 @@ public class ChatService {
 
     private final CommunityApi communityApi;
     private final ChatManager chatManager;
-    private final ChatMessageProducer chatMessageProducer;
-    private final ApplicationEventPublisher eventPublisher;
+    private final ChatStreamWriter chatStreamWriter;
+    private final RedisMessage
 
     public Long enterRoom(Long facilityId, Long userId) {
         Long communityManagerUserId = communityApi.getCommunityManagerUserId(facilityId);
         if (Objects.equals(communityManagerUserId, userId)) throw new CustomException(ErrorCode.INVALID_CHAT_ROOM, "ChatService.enterRoom");
-        Long chatRoomId = chatManager.getOrCreate(facilityId, userId, communityManagerUserId);
-        eventPublisher.publishEvent(new ChatRoomCreatedEvent(this, chatRoomId));
-        return chatRoomId;
+        return chatManager.getOrCreate(facilityId, userId, communityManagerUserId);
     }
 
     public void chat(ChatMessage chatMessage) {
+        chatStreamWriter.write(chatMessage);
 
     }
 }

7a67d018a2d04461756a574e9b5816d3129014b3
gujunhyeong <rnwnsgud90@naver.com>
2025-11-03 19:20:27 +0900

    [GH-10] 순환참조 임시 주석처리

--- gh-domain/gh-chat/gh-chat-core/src/main/java/kr/co/goldenhome/messaging/ChatMessageSubscriber.java
+++ gh-domain/gh-chat/gh-chat-core/src/main/java/kr/co/goldenhome/messaging/ChatMessageSubscriber.java
@@ -12,6 +12,7 @@ import kr.co.goldenhome.repository.ChatConnectionRepository;
 import kr.co.goldenhome.repository.ChatUserRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.messaging.simp.SimpMessageSendingOperations;
 import org.springframework.stereotype.Component;
 
@@ -24,30 +25,27 @@ public class ChatMessageSubscriber {
 
     private final ObjectMapper objectMapper;
     private final SimpMessageSendingOperations messagingTemplate;
-    private final ChatUserRepository chatUserRepository;
-    private final ChatConnectionRepository chatConnectionRepository;
-    private final UserApi userApi;
-    private final FcmSender fcmSender;
+    private final ApplicationEventPublisher eventPublisher;
 
     public void send(String publishedMessage) {
         try {
             ChatMessage chatMessage = objectMapper.readValue(publishedMessage, ChatMessage.class);
             Long chatRoomId = chatMessage.getChatRoomId();
-            Long senderId = chatMessage.getSenderId();
             messagingTemplate.convertAndSend("/topic/chat/" + chatRoomId, chatMessage);
-            List<Long> list = chatUserRepository.findByChatRoomId(chatRoomId).stream()
-                    .map(chatUser -> {
-                        Long userId = chatUser.getUserId();
-                        if (!chatConnectionRepository.isUserViewingChatRoom(userId, chatRoomId)) return userId;
-                        return null;
-                    }).toList();
-            List<String> fcmTokens = userApi.getFcmTokens(list);
-            String userName = userApi.getUserName(senderId);
-            fcmSender.sendMessages(new NotificationsRequest(
-                    fcmTokens,
-                    userName,
-                    publishedMessage
-            ));
+//            eventPublisher.publishEvent(new ChatNotificationEvent()); // todo 이벤트로 분리
+//            List<Long> list = chatUserRepository.findByChatRoomId(chatRoomId).stream()
+//                    .map(chatUser -> {
+//                        Long userId = chatUser.getUserId();
+//                        if (!chatConnectionRepository.isUserViewingChatRoom(userId, chatRoomId)) return userId;
+//                        return null;
+//                    }).toList();
+//            List<String> fcmTokens = userApi.getFcmTokens(list);
+//            String userName = userApi.getUserName(senderId);
+//            fcmSender.sendMessages(new NotificationsRequest(
+//                    fcmTokens,
+//                    userName,
+//                    publishedMessage
+//            ));
 
         } catch (JsonProcessingException e) {
             throw new CustomException(ErrorCode.JSON_PROCESSING_EXCEPTION, "ChatMessageSubscriber.send");

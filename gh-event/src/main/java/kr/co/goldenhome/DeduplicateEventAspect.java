87db840a7cb3a0f58d8541019cfc300354e5f725
gujunhyeong <rnwnsgud90@naver.com>
2026-02-03 19:27:11 +0900

    [GH-13] 이벤트 중복소비 방지

--- gh-event/src/main/java/kr/co/goldenhome/DeduplicateEventAspect.java
+++ gh-event/src/main/java/kr/co/goldenhome/DeduplicateEventAspect.java
@@ -1,12 +1,14 @@
 package kr.co.goldenhome;
 
-import kr.co.goldenhome.event.EventDeduplicationLog;
-import kr.co.goldenhome.event.EventDeduplicationLogRepository;
+import kr.co.goldenhome.event.EventDeduplicationManager;
 import kr.co.goldenhome.model.FacilityEvent;
 import lombok.RequiredArgsConstructor;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -15,7 +17,8 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 public class DeduplicateEventAspect {
 
-    private final EventDeduplicationLogRepository eventDeduplicationLogRepository;
+    private final EventDeduplicationManager eventDeduplicationManager;
+    private static final Logger log = LoggerFactory.getLogger("api-history");
 
     @Transactional
     @Around("@annotation(kr.co.goldenhome.DeduplicateEvent)")
@@ -24,10 +27,13 @@ public class DeduplicateEventAspect {
         FacilityEvent event = (FacilityEvent) args[0];
         String eventId = event.getEventId();
         String id = eventId + "-" + joinPoint.getSignature().getName();
-        if (eventDeduplicationLogRepository.existsById(id)) return null;
-        Object result = joinPoint.proceed();
-        eventDeduplicationLogRepository.save(EventDeduplicationLog.create(id));
-        return result;
+        try {
+            eventDeduplicationManager.saveLog(id);
+        } catch (DataIntegrityViolationException e) {
+            log.info("Duplicate event detected and blocked: {}", id);
+            return null;
+        }
+        return joinPoint.proceed();
     }
 
 }
